81f0e6df13a1d742c4d5d176e7b86ad6f0b094dd
Vira Zhuhan <115663875+ViraZhuhan@users.noreply.github.com>
2023-09-30 08:09:27 +0200

    add start files

---
+++ Screens/RegistrationScreen.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const RegistrationScreen = () => {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        height: 100,
+        padding: 20,
+      }}>
+      <Text>Реєстрація</Text>
+    <Text>Зареєстуватися</Text>
+    </View>
+  );
+};
+
+export default RegistrationScreen;
